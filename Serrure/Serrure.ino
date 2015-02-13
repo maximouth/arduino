767b67de7d16464b05b299dce5a8a0e5e17c04c8
Moi-meme <maximouth@localhost.localdomain>
2015-02-13 20:04:33 +0100

    debut serrure

---
+++ Serrure/Serrure.ino
@@ -0,0 +1,30 @@
+
+//alimentation du potentiometre
+const int pot = 6;
+
+//potentiometre branché sur ax analog
+
+// les 4 valeurs du coffres
+int code[4];
+
+//connaitre la position dans l'automate
+int cpt = 0;
+// valeur du potentiomatre
+int val = 0;
+//temps pour determiner les differants changements
+unsigned long time1 = 0;
+unsigned long time2 = 0;
+
+
+void setup() {
+ Serial.begin(9600);
+ digitalWrite(pot,HIGH);
+ time1 = millis();
+}
+
+void loop() {
+  val = analogRead(1);
+  
+  
+  
+}
