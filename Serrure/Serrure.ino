a950a2263e345e1368e5ced6167f61868c2a5c7b
Moi-meme <maximouth@localhost.localdomain>
2015-02-13 20:21:45 +0100

    serrure premier essais

--- Serrure/Serrure.ino
+++ Serrure/Serrure.ino
@@ -1,3 +1,7 @@
+//temps max avant remise a 0
+#define TMAX 10000
+//temps pour rentrer un chiffre
+#define TCOMB 5000
 
 //alimentation du potentiometre
 const int pot = 6;
@@ -24,7 +28,64 @@ void setup() {
 
 void loop() {
   val = analogRead(1);
-  
-  
-  
+  time2 = millis();
+
+  //si 10 secondes se sont ecoulées sans que personne y touche remise a 0
+  if ((time2-time1) > TMAX){
+    cpt = 0;
+    time1 = millis();
+  }
+  //sinon faire ce que l'on doit faire
+  else {
+    switch (cpt) {
+      //premier chiffre
+    case 0:
+      Serial.println("entrez premier chiffre :");
+      test();
+      break;
+      
+      //second
+    case 1 :
+      Serial.println("entrez second chiffre :");
+      test();
+      break;
+
+      //3eme
+    case 2 :
+      Serial.println("entrez 3eme chiffre :");
+      test();
+      break;
+
+      //dernier
+    case 3 :
+      Serial.println("Et le dernier :");
+      //tester si bon et ouvrir
+      if (test() == 1 ) {
+	Serial.println("bienjoué");
+	//allumer led verte
+      }
+      else {
+	Serial.println("Bien joué mais raté");
+	cpt = 0;
+      }
+      break; 
+    }
+    
+  }
+}
+
+int test() {
+  if ((time2 - time1) > TCOMB) {
+    if (val == code[cpt]) {
+      cpt ++;
+      time1 = millis();
+      return 1;
+    }
+    else {
+      Serial.println("mauvaise entrée, veuillez recommencer");
+      cpt == 0;
+      time2 = millis();
+      return 0;
+    }
+  } 
 }
